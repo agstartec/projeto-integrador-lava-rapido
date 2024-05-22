e6b9065e6956a9a34f7beb2b3a748bc82d5ca841
agstartec <agstartec@gmail.com>
2024-05-22 12:10:50 -0300

    firt commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Lista para armazenar os agendamentos
+agendamentos = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/agendar', methods=['POST'])
+def agendar():
+    if request.method == 'POST':
+        veiculo = request.form['veiculo']
+        placa = request.form['placa']
+        servico = request.form['servico']
+        endereco = request.form['endereco']
+        nome = request.form['nome']
+        telefone = request.form['telefone']
+        data_hora = request.form['data_hora']
+        
+        agendamentos.append({
+            'veiculo': veiculo,
+            'placa': placa,
+            'servico': servico,
+            'endereco': endereco,
+            'nome': nome,
+            'telefone': telefone,
+            'data_hora': data_hora
+        })
+        return redirect(url_for('index'))
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', agendamentos=agendamentos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
