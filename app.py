ba13268007f3ff260f332d56d4b5c0cf513e6502
agstartec <agstartec@gmail.com>
2024-05-26 09:57:23 -0300

    feat:add table agendamentos and return list

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-import pandas as pd
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -19,8 +17,21 @@ class Usuarios(db.Model):
     senha = db.Column(db.String(50), nullable=False)
     rule = db.Column(db.Integer, default=0, nullable=False)
 
-    def __init__(self, id, user, senha, rule):
+    def __init__(self):
         return f'Usuario(id={self.id}, nome={self.user}, email={self.senha})'
+class Agendamentos(db.Model):
+    __tablename__= "agendamentos"
+    id = db.Column(db.Integer, primary_key=True)
+    veiculo = db.Column(db.String(45), nullable=False)
+    placa = db.Column(db.String(45), nullable=False)
+    servico = db.Column(db.String(45), nullable=False)
+    endereco = db.Column(db.String(45), nullable=False)
+    nome = db.Column(db.String(45), nullable=False)
+    telefone = db.Column(db.String(45), nullable=False)
+    data = db.Column(db.Date, nullable=False)
+    hora = db.Column(db.String(45), nullable=False)
+    # def __init__(self):
+    #     return f'Agendamento(id={self.id}, veiculo={self.veiculo}, placa={self.placa}, servico={self.servico}, endereco={self.endereco}, nome={self.nome}, telefone={self.telefone}, data_hora={self.data_hora})'
 
 # Lista para armazenar os agendamentos
 with app.app_context():
@@ -28,7 +39,6 @@ with app.app_context():
     usuarios = Usuarios.query.all()
     for usuario in usuarios:
         print(f'ID: {usuario.id}, Nome: {usuario.user}, Email: {usuario.senha}')
-agendamentos = []
 
 @app.route('/')
 def index():
@@ -66,21 +76,27 @@ def agendar():
         endereco = request.form['endereco']
         nome = request.form['nome']
         telefone = request.form['telefone']
-        data_hora = request.form['data_hora']
+        hora = request.form['data_hora']
+        data = request.form['data']
         
-        agendamentos.append({
-            'veiculo': veiculo,
-            'placa': placa,
-            'servico': servico,
-            'endereco': endereco,
-            'nome': nome,
-            'telefone': telefone,
-            'data_hora': data_hora
-        })
-        return redirect(url_for('index'))
+        novo_agendamento = Agendamentos(
+            veiculo=veiculo,
+            placa=placa,
+            servico=servico,
+            endereco=endereco,
+            nome=nome,
+            telefone=telefone,
+            data=data,
+            hora=hora
+        )
+        db.session.add(novo_agendamento)
+        db.session.commit()
+
+        return redirect(url_for('painel'))
 
 @app.route('/admin')
 def admin():
+    agendamentos = Agendamentos.query.all()
     return render_template('admin.html', agendamentos=agendamentos)
 
 @app.route('/painel')
